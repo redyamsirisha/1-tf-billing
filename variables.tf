a4af9d95bee607544e9bb8a07e43da87b8397cee
redyamsirisha <73577330+redyamsirisha@users.noreply.github.com>
2021-10-12 23:04:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
-variable = 
+variable "project_name" {
+  type = string
+  default = "Gcp-Service"
+}
+
+variable "projcet_id" {
+  type = string
+  default = "gcp-service-323614"
+}
+
+variable "billing_account" {
+type = string
+default = "017751-06169F-845897"
+}
